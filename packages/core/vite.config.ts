dd310f9d8ef7e0004a611a04e4573a87168407ad
drumath2237 <2237k.drumath@gmail.com>
2023-10-25 02:37:34 +0900

    :wrench: vite configのコメントアウトを解除

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-// import dts from "vite-plugin-dts";
-// import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
 export default defineConfig(({ mode }) => {
   // for github pages
@@ -9,28 +9,28 @@ export default defineConfig(({ mode }) => {
     return {
       plugins: [
         vue(),
-        // dts({
-        //   include: ["src/**/**.ts", "src/**/**.vue"],
-        //   exclude: ["src/main.ts", "**/**.svg"],
-        // }),
+        dts({
+          include: ["src/**/**.ts", "src/**/**.vue"],
+          exclude: ["src/main.ts", "**/**.svg"],
+        }),
       ],
-      // build: {
-      //   lib: {
-      //     entry: resolve(__dirname, "src/index.ts"),
-      //     name: "vue-babylon-investigation",
-      //     fileName: "index",
-      //     formats: ["es", "umd"],
-      //   },
-      //   rollupOptions: {
-      //     external: ["vue", "@babylonjs/core"],
-      //     output: {
-      //       globals: {
-      //         vue: "Vue",
-      //         "@babylonjs/core": "BABYLON",
-      //       },
-      //     },
-      //   },
-      // },
+      build: {
+        lib: {
+          entry: resolve(__dirname, "src/index.ts"),
+          name: "vue-babylon-investigation",
+          fileName: "index",
+          formats: ["es", "umd"],
+        },
+        rollupOptions: {
+          external: ["vue", "@babylonjs/core"],
+          output: {
+            globals: {
+              vue: "Vue",
+              "@babylonjs/core": "BABYLON",
+            },
+          },
+        },
+      },
     };
   }
 
